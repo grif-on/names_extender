fe46836dda67b74d08be0907c5a22b27453fc644
Grif_on <91139654+grif-on@users.noreply.github.com>
2024-03-11 16:39:35 +0300

    implemented ignore_extension argument

--- names_extender.js
+++ names_extender.js
@@ -1,14 +1,21 @@
-let [directory_path, desired_length] = process.argv.slice(2);
+let [directory_path, desired_length, ignore_extension] = process.argv.slice(2);
 
 if (directory_path === undefined) {
   console.log("Can't work without an arguments !")
-  console.log("Usage : node names_extender.js <directory_path> [desired_length]")
+  console.log("Usage : node names_extender.js <directory_path> [desired_length] [ignore_extension]")
   process.exit(1)
 }
 
 if (desired_length === undefined) {
   desired_length = 9
   console.log("Using default desired_length length : " + desired_length)
+}
+
+if (ignore_extension === undefined) {
+  ignore_extension = true
+  console.log("Using default ignore_extension value : " + ignore_extension)
+} else {
+  ignore_extension = (ignore_extension.toLowerCase() === "true")
 }
 
 let fs = require("fs");
@@ -19,7 +26,8 @@ fs.readdir(directory_path, (err, files) => {
     process.exit(2)
   }
   files.forEach((file_name) => {
-    file_name = file_name.padStart(desired_length, "X")
+    let additional_length = (ignore_extension) ? (file_name.length - file_name.lastIndexOf(".")) : 0
+    file_name = file_name.padStart(desired_length + additional_length, "X")
     console.log(file_name)
   })
 })
