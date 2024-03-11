7b2984090f1d22bfad799a6e755383ad5fd6936f
Grif_on <91139654+grif-on@users.noreply.github.com>
2024-03-11 04:38:05 +0300

    Refactored reading of arguments

--- names_extender.js
+++ names_extender.js
@@ -1,23 +1,19 @@
-const arg = process.argv.slice(2)
+let [directory_path, desired_length] = process.argv.slice(2);
 
-if (arg.length < 1) {
+if (directory_path === undefined) {
   console.log("Can't work without an arguments !")
   console.log("Usage : node names_extender.js <directory_path> [desired_length]")
   process.exit(1)
 }
 
-
-let desired_length = 9
-
-if (arg.length < 2) {
+if (desired_length === undefined) {
+  desired_length = 9
   console.log("Using default desired_length length : " + desired_length)
-} else {
-  desired_length = arg[1]
 }
 
 let fs = require("fs");
 //note - readdir is asynchronous
-fs.readdir(arg[0], (err, files) => {
+fs.readdir(directory_path, (err, files) => {
   if (err) {
     console.log(err)
     process.exit(2)
