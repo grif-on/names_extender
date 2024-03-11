316c575aff1f0c7fcdae4c39b790f0ce28858315
Grif_on <91139654+grif-on@users.noreply.github.com>
2024-03-11 20:06:56 +0300

    Refactored default arguments and rewriten docs

--- names_extender.js
+++ names_extender.js
@@ -1,14 +1,34 @@
-let [directory_path, extender_symbol, desired_length, ignore_extension] = process.argv.slice(2);
+let defaultArguments = {
+  extender_symbol: "0",
+  desired_length: 9,
+  ignore_extension: true
+}
+
+function printUsage() {
+  console.log("Usage : node names_extender.js <directory_path> [extender_symbol] [desired_length] [ignore_extension]\n")
+  
+  console.log("@param {string} directory_path - the path to the directory in wich to extend the names of the files .")
+  console.log("@param {string} extender_symbol - the symbol to extend the filenames with . The default value is \"" + defaultArguments.extender_symbol + "\" .")
+  console.log("@param {number} desired_length - the length of the filenames after extending . The default value is " + defaultArguments.desired_length + " .")
+  console.log("@param {boolean} ignore_extension - desired_length should not include the extension length . The default value is " + defaultArguments.ignore_extension + " .")
+}
+
+
+let [directory_path, extender_symbol, desired_length, ignore_extension] = process.argv.slice(2)
 
 if (directory_path === undefined) {
-  console.log("Can't work without an arguments !")
-  console.log("Usage : node names_extender.js <directory_path> [extender_symbol] [desired_length] [ignore_extension]")
+  console.log("Can't work without directory_path argument !\n")
+  printUsage()
   process.exit(1)
 }
 
+if (directory_path === "-h" || directory_path === "--help" || directory_path === "/?") {
+  printUsage()
+  process.exit(0)
+}
+
 if (extender_symbol === undefined) {
-  extender_symbol = "0"
-  console.log("Using default extender_symbol value : \"" + extender_symbol + "\"")
+  extender_symbol = defaultArguments.extender_symbol
 } else {
   if (extender_symbol.length > 1) {
     console.log("Note you supplied a multiple symbols to extend filenames with : " + "\"" + extender_symbol + "\"")
@@ -16,8 +36,7 @@ if (extender_symbol === undefined) {
 }
 
 if (desired_length === undefined) {
-  desired_length = 9
-  console.log("Using default desired_length length : " + desired_length)
+  desired_length = defaultArguments.desired_length
 } else {
   desired_length = parseInt(desired_length)
   if (isNaN(desired_length)) {
@@ -27,8 +46,7 @@ if (desired_length === undefined) {
 }
 
 if (ignore_extension === undefined) {
-  ignore_extension = true
-  console.log("Using default ignore_extension value : " + ignore_extension)
+  ignore_extension = defaultArguments.ignore_extension
 } else {
   ignore_extension = (ignore_extension.toLowerCase() === "true")
 }
